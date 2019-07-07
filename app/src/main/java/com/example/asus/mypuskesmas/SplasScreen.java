73e36a8588015277f2a21731ce42f6dc471f61a0
Asus <fardanalgi@gmail.com>
2019-07-07 09:34:37 +0700

    add splashscreen

--- app/src/main/java/com/example/asus/mypuskesmas/SplasScreen.java
+++ app/src/main/java/com/example/asus/mypuskesmas/SplasScreen.java
@@ -1,5 +1,6 @@
 package com.example.asus.mypuskesmas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,19 @@ public class SplasScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splas_screen);
+
+        Thread thread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    startActivity(new Intent(SplasScreen.this,MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
     }
 }
